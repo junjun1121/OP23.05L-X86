12c898af7e3af1c04a51d362ff2c4bc5ede4935b
junjun1121 <136947888+junjun1121@users.noreply.github.com>
2023-07-08 15:12:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,11 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/packages/net/xray-core
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone https://github.com/sbwml/luci-app-alist package/alist
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/psw
+git clone -b packages https://github.com/xiaorouji/openwrt-passwall package/pswcore
